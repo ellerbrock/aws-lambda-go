c4c0a73c331a9e04be166f74104db9cd0c3309de
Maik Ellerbrock <opensource@frapsoft.com>
2018-01-21 08:53:02 +0100

    chore(git): initial commit

--- main.go
+++ main.go
@@ -1,29 +1,15 @@
 package main
 
 import (
-	"io/ioutil"
+	"log"
 
-	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-
-	index, err := ioutil.ReadFile("public/index.html")
-	if err != nil {
-		return events.APIGatewayProxyResponse{}, err
-	}
-
-	return events.APIGatewayProxyResponse{
-		StatusCode: 200,
-		Body:       string(index),
-		Headers: map[string]string{
-			"Content-Type": "text/html",
-		},
-	}, nil
-
+func HandleRequest() {
+	log.Print("Hello Lambda, I'm here to rock!")
 }
 
 func main() {
-	lambda.Start(handler)
+	lambda.Start(HandleRequest)
 }
